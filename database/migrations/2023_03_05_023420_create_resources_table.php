76208c2bee600d4ac2cb5fd3fa2d693958e99a99
Arthur Warmling <arthurwarmling1@gmail.com>
2023-03-05 02:51:24 -0300

    feat: roles and resources;

---
+++ database/migrations/2023_03_05_023420_create_resources_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('resources', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('resource');
+            $table->boolean('is_menu')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('resources');
+    }
+};
